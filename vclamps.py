9cd7c9e511018e814e6dbdde208f4ac0aca0dedd
Padraig Gleeson <p.gleeson@gmail.com>
2018-05-16 16:23:38 +0100

    Improved limits

--- vclamps.py
+++ vclamps.py
@@ -5,7 +5,8 @@ import numpy as np
 
 # v_clamps_i_seg0_EPSC.12345.dat  v_clamps_i_seg0_IPSC.12345.dat  v_clamps_i_seg1406_EPSC.12345.dat  v_clamps_i_seg1406_IPSC.12345.dat  v_clamps_i_seg2953_EPSC.12345.dat  v_clamps_i_seg2953_IPSC.12345.dat
 
-lims = {'EPSC':(-1e-8,1e-9),'IPSC':(0.8e-8,1.4e-8)}
+lims = {'EPSC':(-0.16e-8,0.15e-9),'IPSC':(0.9e-8,1.2e-8)}
+lims2 = {'EPSC':(-7e-11,0e-10),'IPSC':(0e-8,1.7e-10)}
 
 
 for x in lims.keys():
@@ -15,6 +16,7 @@ for x in lims.keys():
     ys = []
     labels = []
     all_seg_0 = []
+    all_seg_1406 = []
     for f in os.listdir('./temp'):
         if f.startswith('v_clamps') and x in f and f.endswith('dat'):
             print('Adding %s'%f)
@@ -28,14 +30,22 @@ for x in lims.keys():
             labels.append('%s %s'%(seg,seed))
             if seg == 'seg0':
                 all_seg_0.append(res[0])
+            if seg == 'seg1406':
+                all_seg_1406.append(res[0])
+                
     
     avg_seg0 = np.zeros(len(all_seg_0[0]))
     for l in all_seg_0:
         for i in range(len(l)):
             avg_seg0[i]+=l[i]
             
-
             
+    avg_seg1406 = np.zeros(len(all_seg_1406[0]))
+    for l in all_seg_1406:
+        for i in range(len(l)):
+            avg_seg1406[i]+=l[i]
+            
+ 
     pynml.generate_plot(xs,
                         ys,
                         'V clamp currents %s'%x,
@@ -55,6 +65,22 @@ for x in lims.keys():
                         ys,
                         'V clamp current averages at soma %s'%x,
                         ylim=lims[x],
+                        labels=labels,
+                        show_plot_already=False)
+    
+    xs = []
+    ys = []
+    labels = []
+    
+    xs.append(time_points)
+    ys.append(avg_seg1406/len(all_seg_1406))
+    seg = 'seg1406'
+    labels.append('%s average'%(seg))
+    
+    pynml.generate_plot(xs,
+                        ys,
+                        'V clamp current averages at seg1406 (dendrite) %s'%x,
+                        ylim=lims2[x],
                         labels=labels,
                         show_plot_already=False)
     
